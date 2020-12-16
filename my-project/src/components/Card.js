fb32561d5d4fd536cfe2f60db7fb72140c8fc816
123aadina <adina.hamida@gmail.com>
2020-12-16 14:03:38 +0100

    map is working

--- my-project/src/components/Card.js
+++ my-project/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({item}) => {
   return (
     <div>
       <div className="flip-card">
@@ -9,7 +9,7 @@ const Card = () => {
             <img src="img_avatar.png" alt="Avatar" />
           </div>
           <div className="flip-card-back">
-            <h1>hi</h1>
+            <h1>{item.name}</h1>
             <p>Architect & Engineer</p>
             <p>We love that guy</p>
           </div>
--- my-project/src/components/Home.js
+++ my-project/src/components/Home.js
@@ -1,34 +1,37 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import  Card from './Card';
 
 const Home = () => {
-    const [data, setData] = useState()
+  const [data, setData] = useState();
 
-    useEffect(() => {
-     fetchData()
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    }, []);
+  const fetchData = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character/");
+    const data = await response.json();
 
-    const fetchData = async () => {
-        const response = await fetch('https://rickandmortyapi.com/api/character/')
-        const data = await response.json()
+    console.log(data.results);
+    setData(data.results);
 
-        console.log(data)
-        setData(data)
-
-    };
-    
-
+    console.log(data , 'DATA');
+  };
 
   return (
     <div>
       <input></input>
       <button>Search</button>
-
-     
+      <h1>List</h1>
+      {data && data.map((item) => {
+          return (
+            <Card item={item} key={item._id} />
+             
+          )
+      })}
     </div>
   );
 };
 export default Home;
-
 
 /* style="width:300px;height:300px;" */
