30f637477bf63a66c6aa3cc83f571ffe43f36e12
123aadina <adina.hamida@gmail.com>
2020-12-16 18:33:53 +0100

    bootstap installed

--- my-project/src/components/Card.js
+++ my-project/src/components/Card.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+
+
 const Card = ({item}) => {
 
   const handleClick = (e) => {
     e.preventDefault();
         console.log('clicked')
-    }
+    };
+
+
   return (
-    <div className="container">
-      <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
+    <div>
+      <div style = {style}>
+        <div style = {myStyle}>
+          <div >
             <img src={item.image} alt="Avatar" />
           </div>
           <div className="flip-card-back">
@@ -23,4 +27,46 @@ const Card = ({item}) => {
   );
 };
 
+const style = {
+    backgroundColor: 'transparent',
+    width: '300px',
+    height: '200px',
+    border: '1px solid #f1f1f1',
+    perspective: '1000px',
+};
+
+
+const myStyle = {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    textAlign: 'center',
+    transition: 'transform 0.8s',
+    transformStyle:' preserve-3d'
+}
+
+
+
+/* const hoverStyle = {
+
+hover:
+    transform: 'rotateY(180deg)',
+  } */
 export default Card;
+
+
+
+/* 
+<div style = {style}>
+<div className="flip-card">
+  <div className="flip-card-inner">
+    <div className="flip-card-front">
+      <img src={item.image} alt="Avatar" />
+    </div>
+    <div className="flip-card-back">
+      <h1>{item.name}</h1>
+      <button onClick= {handleClick}>Learn more</button>
+    </div>
+  </div>
+</div>
+</div> */
