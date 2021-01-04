42b5bca2c1676a3298d1cb2c6eb03f13418b614a
123aadina <adina.hamida@gmail.com>
2021-01-04 14:47:22 +0100

    made a new component for search bar

---
+++ my-project/src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <div className="baner">
+        <div className="header">
+          <button>Search</button>
+        </div>
+      </div>
+      hi
+    </div>
+  );
+};
+
+export default Search;
